9e0e7e9cc22d40d787c511496788718de6186302
Labib Miah <labibmiah05@gmail.com>
2025-11-06 18:31:01 +0000

    DB

---
+++ app/Store/Donor/page.tsx
@@ -0,0 +1,9 @@
+export default function Donor() {
+  return (
+    <div>
+      <title>Donor Page</title>
+      <h2>Make a Donation</h2>
+      <footer>This is the footer</footer>
+    </div>
+  );
+}
---
+++ app/Store/page.tsx
@@ -0,0 +1,9 @@
+export default function Store() {
+  return (
+    <div>
+      <title>Store Page</title>
+      <h2>Welcome to the Store </h2>
+      <footer>This is the footer</footer>
+    </div>
+  );
+}
