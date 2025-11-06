169bff6527c4679a7b97aa54d92076a533178483
Labib Miah <labibmiah05@gmail.com>
2025-11-06 18:58:05 +0000

    Updated FrontEnd With DB

---
+++ app/Store/Registar/page.tsx
@@ -0,0 +1,9 @@
+export default function Registar() {
+  return (
+    <div>
+      <title>Registar Page</title>
+      <h2>Registar</h2>
+      <footer>This is the footer</footer>
+    </div>
+  );
+}
